2e3ac1c82596c5c82f2598a0a243b1ceddd80782
Anthony Li <me@anli.dev>
2022-02-17 09:33:28 -0500

    Update "14-hour hackathon" to "2-day hackathon"

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </Head>
         <Navbar />
         <HeroHeader />
-        <Section title="A 14-hour hackathon">
+        <Section title="A 2-day hackathon">
             <div className="flex text-center sm:text-left flex-col sm:flex-row items-center py-4 sm:mt-8">
                 <img src="https://v.hackbca.com/images/whatis0.jpg" className="w-full max-w-md rounded-xl mb-4 sm:mb-0 sm:mr-8" />
                 <p className="text-2xl">Hack together a project with friends. Use any language or tool you want. Go wild.</p>
